c75d1fad2702516597fafb2419e07fb82e49fe40
Ash <ssetem@googlemail.com>
2015-01-29 13:52:48 +0000

    fix usage of spur-ioc

--- src/injector.js
+++ src/injector.js
@@ -3,17 +3,14 @@ var spur = require("spur-ioc")
 module.exports = function(){
   var ioc = spur.create("expressModule");
 
-  ioc.registerLibraries({
-    "express"         : "express",
-    "methodOverride"  : "method-override",
-    "cookieParser"    : "cookie-parser",
-    "bodyParser"      : "body-parser",
-    "path"            : "path"
-  });
-
   ioc.registerDependencies({
-    "console"     :console,
-    "nodeProcess" :process
+    "express"         : require("express"),
+    "methodOverride"  : require("method-override"),
+    "cookieParser"    : require("cookie-parser"),
+    "bodyParser"      : require("body-parser"),
+    "path"            : require("path"),
+    "console"         : console,
+    "nodeProcess"     : process
   });
 
   ioc.registerFolders(__dirname, [
